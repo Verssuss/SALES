68a85f16846175ef57244e7e96d893e6808a44d9
Mihail <rozhkovmisha_201111@mail.ru>
2021-06-25 14:01:42 +0700

    Structure Project

---
+++ SALES/Models/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SALES.Models
+{
+    class Sale
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public DateTime SaleDate { get; set; }
+
+    }
+}
